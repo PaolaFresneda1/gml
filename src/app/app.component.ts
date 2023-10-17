db9e2bedd0feb6a8755badeb0976fd765a670bc9
Paolafresneda1 <paolafresneda1@gmail.com>
2023-10-17 09:10:04 -0500

    Formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Product } from './models/product';
 })
 export class AppComponent {
   productArray: Product[] = [
-    {id: 1, logo: "Logo 1", nombreProducto: "Producto 1", descripcion: "Descripcion 1", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" },
-    {id: 2, logo: "Logo 2", nombreProducto: "Producto 2", descripcion: "Descripcion 2", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" },
-    {id: 3, logo: "Logo 3", nombreProducto: "Producto 3", descripcion: "Descripcion 3", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" },
-    {id: 4, logo: "Logo 4", nombreProducto: "Producto 4", descripcion: "Descripcion 4", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" }
+    {id: 1, logo: "Logo 1", nombreProducto: "Producto 1", descripcion: "Descripcion 1", fechaLiberacion: "2023-10-12", fechaReestruturacion: "2023-10-12" },
+    {id: 2, logo: "Logo 2", nombreProducto: "Producto 2", descripcion: "Descripcion 2", fechaLiberacion: "2023-10-12", fechaReestruturacion: "2023-10-12" },
+    {id: 3, logo: "Logo 3", nombreProducto: "Producto 3", descripcion: "Descripcion 3", fechaLiberacion: "2023-10-12", fechaReestruturacion: "2023-10-12" },
+    {id: 4, logo: "Logo 4", nombreProducto: "Producto 4", descripcion: "Descripcion 4", fechaLiberacion: "2023-10-12", fechaReestruturacion: "2023-10-12" }
   ];
 
   selectedProduct: Product = new Product();

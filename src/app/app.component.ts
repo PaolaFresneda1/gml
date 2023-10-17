d8329e255fe08db194b7c7f37b09ba25c588b028
Paolafresneda1 <paolafresneda1@gmail.com>
2023-10-17 00:25:49 -0500

    Proyecto crud sin estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba-gml';
+  productArray: Product[] = [
+    {id: 1, logo: "Logo 1", nombreProducto: "Producto 1", descripcion: "Descripcion 1", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" },
+    {id: 2, logo: "Logo 2", nombreProducto: "Producto 2", descripcion: "Descripcion 2", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" },
+    {id: 3, logo: "Logo 3", nombreProducto: "Producto 3", descripcion: "Descripcion 3", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" },
+    {id: 4, logo: "Logo 4", nombreProducto: "Producto 4", descripcion: "Descripcion 4", fechaLiberacion: "12/10/2023", fechaReestruturacion: "12/10/2023" }
+  ];
+
+  selectedProduct: Product = new Product();
+
+  addOrEdit(){
+
+    if(this.selectedProduct.id === 0){
+      this.selectedProduct.id = this.productArray.length + 1;
+      this.productArray.push(this.selectedProduct);
+    }
+    this.selectedProduct = new Product();
+  }
+
+  openForEdit(product: Product){
+    this.selectedProduct = product;
+  }
+
+  delete(){
+    if (confirm('¿Esta seguro de eliminar este producto?')) {
+      this.productArray = this.productArray.filter(element => element != this.selectedProduct);
+      this.selectedProduct = new Product();
+    }
+    
+  }
+
+  
 }
